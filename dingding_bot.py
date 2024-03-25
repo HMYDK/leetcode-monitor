008515aa85bc1cb02786333ee8f4e39aa211a741
HMYDK <paranoia_zk@yeah.net>
2024-03-25 10:07:09 +0800

    config : 补充配置文件

--- dingding_bot.py
+++ dingding_bot.py
@@ -3,9 +3,17 @@ import json
 
 import requests
 
+CONFIG_FILE = 'config.json'
+
+
+def load_config():
+    with open(CONFIG_FILE) as f:
+        return json.load(f)
+
 
 def send(msg: str):
-    webhook = "https://oapi.dingtalk.com/robot/send?access_token=ed5117bd15d23ce4d52c77d284e6f461de874ffcd5782177da7c9cabd0d514da"
+    config = load_config()
+    webhook_url = config['webhook_url']
 
     # 构造消息体
     header = {
@@ -20,7 +28,7 @@ def send(msg: str):
 
     # 发送请求
     try:
-        r = requests.post(url=webhook, headers=header, data=message.encode("utf-8"))
+        r = requests.post(url=webhook_url, headers=header, data=message.encode("utf-8"))
         print(r.text)
     except Exception as e:
         print(e)
